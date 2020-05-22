22126840e9674e9af49ab4f7636898d15288ebcd
raphael <raphael@cloud13.de>
2020-05-22 14:43:10 +0200

    fix uploading images to the whiteboard

--- scripts/server-backend.js
+++ scripts/server-backend.js
@@ -26,7 +26,6 @@ function startBackendServer(port) {
     console.log("Webserver & socketserver running on port:" + port);
 
     const { accessToken, enableWebdav } = config.backend;
-    const { imageDownloadFormat } = config.frontend;
 
     app.get("/api/loadwhiteboard", function (req, res) {
         var wid = req["query"]["wid"];
@@ -92,7 +91,7 @@ function startBackendServer(port) {
 
         var name = fields["name"] || "";
         var date = fields["date"] || +new Date();
-        var filename = whiteboardId + "_" + date + "." + imageDownloadFormat;
+        var filename = whiteboardId + "_" + date + ".png";
         var webdavaccess = fields["webdavaccess"] || false;
         try {
             webdavaccess = JSON.parse(webdavaccess);

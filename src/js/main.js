9d55395e0db850083da3bfa853c473b4ac41f9e2
raphael <raphael@cloud13.de>
2021-02-11 15:33:08 +0100

    fix pdf import

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ import whiteboard from "./whiteboard";
 import keybinds from "./keybinds";
 import Picker from "vanilla-picker";
 import { dom } from "@fortawesome/fontawesome-svg-core";
-import pdfjsLib from "pdfjs-dist";
+//import pdfjsLib from "pdfjs-dist/webpack";
 import shortcutFunctions from "./shortcutFunctions";
 import ReadOnlyService from "./services/ReadOnlyService";
 import InfoService from "./services/InfoService";

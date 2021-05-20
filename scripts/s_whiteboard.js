03c7e0c670c0668fd64a23d883c7aa09cb6fc760
raphael <raphael@cloud13.de>
2021-05-20 17:15:03 +0200

    store all whiteboards in different db files so we use less memory

--- scripts/s_whiteboard.js
+++ scripts/s_whiteboard.js
@@ -4,17 +4,13 @@ const config = require("./config/config");
 
 var savedBoards = {};
 var savedUndos = {};
-var saveDelay = false;
+var saveDelay = {};
 
 if (config.backend.enableFileDatabase) {
-    //read saved boards from file
-    fs.readFile("savedBoards.json", (err, data) => {
-        if (err) {
-            return console.log(
-                "No persistend Whiteboard Datafile found... this is not a problem on the first start!"
-            );
-        }
-        savedBoards = JSON.parse(data);
+    // make sure that folder with saved boards exists
+    fs.mkdirSync("savedBoards", {
+        // this option also mutes an error if path exists
+        recursive: true,
     });
 }
 
@@ -27,12 +23,19 @@ module.exports = {
             //Clear the whiteboard
             delete savedBoards[wid];
             delete savedUndos[wid];
+            // delete the corresponding file too
+            fs.unlink("savedBoards/" + wid + ".json", function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
         } else if (tool === "undo") {
             //Undo an action
             if (!savedUndos[wid]) {
                 savedUndos[wid] = [];
             }
-            if (savedBoards[wid]) {
+            let savedBoard = this.loadStoredData(wid);
+            if (savedBoard) {
                 for (var i = savedBoards[wid].length - 1; i >= 0; i--) {
                     if (savedBoards[wid][i]["username"] == username) {
                         var drawId = savedBoards[wid][i]["drawId"];
@@ -56,9 +59,7 @@ module.exports = {
             if (!savedUndos[wid]) {
                 savedUndos[wid] = [];
             }
-            if (!savedBoards[wid]) {
-                savedBoards[wid] = [];
-            }
+            let savedBoard = this.loadStoredData(wid);
             for (var i = savedUndos[wid].length - 1; i >= 0; i--) {
                 if (savedUndos[wid][i]["username"] == username) {
                     var drawId = savedUndos[wid][i]["drawId"];
@@ -67,7 +68,7 @@ module.exports = {
                             savedUndos[wid][i]["drawId"] == drawId &&
                             savedUndos[wid][i]["username"] == username
                         ) {
-                            savedBoards[wid].push(savedUndos[wid][i]);
+                            savedBoard.push(savedUndos[wid][i]);
                             savedUndos[wid].splice(i, 1);
                         }
                     }
@@ -92,40 +93,82 @@ module.exports = {
                 "setTextboxFontColor",
             ].includes(tool)
         ) {
+            let savedBoard = this.loadStoredData(wid);
             //Save all this actions
-            savedBoards[wid] = savedBoards[wid] ? savedBoards[wid] : [];
             delete content["wid"]; //Delete id from content so we don't store it twice
             if (tool === "setTextboxText") {
-                for (var i = savedBoards[wid].length - 1; i >= 0; i--) {
+                for (var i = savedBoard.length - 1; i >= 0; i--) {
                     //Remove old textbox tex -> dont store it twice
                     if (
-                        savedBoards[wid][i]["t"] === "setTextboxText" &&
-                        savedBoards[wid][i]["d"][0] === content["d"][0]
+                        savedBoard[i]["t"] === "setTextboxText" &&
+                        savedBoard[i]["d"][0] === content["d"][0]
                     ) {
-                        savedBoards[wid].splice(i, 1);
+                        savedBoard.splice(i, 1);
                     }
                 }
             }
-            savedBoards[wid].push(content);
+            savedBoard.push(content);
         }
-
+        this.saveToDB(wid);
+    },
+    saveToDB: function (wid) {
         if (config.backend.enableFileDatabase) {
             //Save whiteboard to file
-            if (!saveDelay) {
-                saveDelay = true;
+            if (!saveDelay[wid]) {
+                saveDelay[wid] = true;
                 setTimeout(function () {
-                    saveDelay = false;
-                    fs.writeFile("savedBoards.json", JSON.stringify(savedBoards), (err) => {
-                        if (err) {
-                            return console.log(err);
-                        }
-                    });
+                    saveDelay[wid] = false;
+                    if (savedBoards[wid]) {
+                        fs.writeFile(
+                            "savedBoards/" + wid + ".json",
+                            JSON.stringify(savedBoards[wid]),
+                            (err) => {
+                                if (err) {
+                                    return console.log(err);
+                                }
+                            }
+                        );
+                    }
                 }, 1000 * 10); //Save after 10 sec
             }
         }
     },
+    // Load saved whiteboard
     loadStoredData: function (wid) {
-        //Load saved whiteboard
-        return savedBoards[wid] ? savedBoards[wid] : [];
+        if (wid in savedBoards) {
+            return savedBoards[wid];
+        }
+
+        savedBoards[wid] = [];
+
+        // try to load from DB
+        if (config.backend.enableFileDatabase) {
+            //read saved board from file
+            var filePath = "savedBoards/" + wid + ".json";
+            if (fs.existsSync(filePath)) {
+                var data = fs.readFileSync(filePath);
+                if (data) {
+                    savedBoards[wid] = JSON.parse(data);
+                }
+            }
+        }
+
+        return savedBoards[wid];
+    },
+    copyStoredData: function (sourceWid, targetWid) {
+        const sourceData = this.loadStoredData(sourceWid);
+        if (sourceData.length === 0 || this.loadStoredData(targetWid).lenght > 0) {
+            return;
+        }
+        savedBoards[targetWid] = sourceData.slice();
+        this.saveToDB(targetWid);
+    },
+    saveData: function (wid, data) {
+        const existingData = this.loadStoredData(wid);
+        if (existingData.length > 0 || !data) {
+            return;
+        }
+        savedBoards[wid] = JSON.parse(data);
+        this.saveToDB(wid);
     },
 };

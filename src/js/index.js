c0e7389570ca9754263011c4328cd6f39546a566
Raphael <raphael@cloud13.de>
2023-03-15 23:36:53 +0100

    fix mobile style

--- src/js/index.js
+++ src/js/index.js
@@ -9,15 +9,9 @@ import "./icons";
 import main from "./main";
 
 $(document).ready(function () {
-    // Set correct width height on mobile browsers
-    const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-    if (isChrome) {
-        $("head").append(
-            '<meta name="viewport" content="width=device-width, initial-scale=0.52, maximum-scale=1" />'
-        );
-    } else {
-        $("head").append('<meta name="viewport" content="width=1400" />');
-    }
+    $("head").append(
+        '<meta name="viewport" content="width=device-width, initial-scale=0.52, maximum-scale=1" />'
+    );
 
     main();
 });

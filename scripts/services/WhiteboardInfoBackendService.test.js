dfcbf682b9fb858cad3345b2001b25388a78900d
Raphael <raphael@cloud13.de>
2026-02-25 16:26:21 +0100

    fix tests

--- scripts/services/WhiteboardInfoBackendService.test.js
+++ scripts/services/WhiteboardInfoBackendService.test.js
@@ -1,4 +1,4 @@
-import ReadOnlyBackendService from "./ReadOnlyBackendService.js";
+import InfoByWhiteBoardMap from "./ReadOnlyBackendService.js";
 import WhiteboardInfoBackendService from "./WhiteboardInfoBackendService.js";
 
 test("Clients lifetime same wid", () => {
@@ -21,29 +21,29 @@ test("Clients lifetime same wid", () => {
 });
 
 test("Clients lifetime both wid and readonly wid", () => {
-    const wid = "2";
+    const wid = "1";
     const service = new WhiteboardInfoBackendService();
-    const rOservice = new ReadOnlyBackendService();
+    const rOservice = new InfoByWhiteBoardMap();
     const readOnlyWid = rOservice.getReadOnlyId(wid);
-    console.log("BLA", readOnlyWid);
+    const widBack = rOservice.getIdFromReadOnlyId(readOnlyWid);
 
     expect(service.getNbClientOnWhiteboard(wid)).toBe(null);
-    expect(service.getNbClientOnWhiteboard(readOnlyWid)).toBe(null);
+    expect(service.getNbClientOnWhiteboard(widBack)).toBe(null);
 
     service.join("toto", wid, null);
     expect(service.getNbClientOnWhiteboard(wid)).toBe(1);
-    expect(service.getNbClientOnWhiteboard(readOnlyWid)).toBe(1);
+    expect(service.getNbClientOnWhiteboard(widBack)).toBe(1);
 
-    service.join("tata", readOnlyWid, null);
+    service.join("tata", wid, null);
     expect(service.getNbClientOnWhiteboard(wid)).toBe(2);
-    expect(service.getNbClientOnWhiteboard(readOnlyWid)).toBe(2);
+    expect(service.getNbClientOnWhiteboard(widBack)).toBe(2);
 
-    service.leave("tata", readOnlyWid, null);
+    service.leave("tata", wid, null);
     expect(service.getNbClientOnWhiteboard(wid)).toBe(1);
-    expect(service.getNbClientOnWhiteboard(readOnlyWid)).toBe(1);
+    expect(service.getNbClientOnWhiteboard(widBack)).toBe(1);
 
     service.leave("toto", wid, null);
     // no more user on whiteboard
     expect(service.getNbClientOnWhiteboard(wid)).toBe(null);
-    expect(service.getNbClientOnWhiteboard(readOnlyWid)).toBe(null);
+    expect(service.getNbClientOnWhiteboard(widBack)).toBe(null);
 });
